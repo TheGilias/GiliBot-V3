d0ad171e9d7f13bd0f1561c2ca6d0188104c2903
bvasser <bvasser@cloudplus.net>
2020-06-11 16:31:21 -0400

    Update streamtypes.py

--- streamclips/streamtypes.py
+++ streamclips/streamtypes.py
@@ -365,14 +365,14 @@ class MixerStream(Stream):
     token_name = None  # This streaming services don't currently require an API key
 
     def get_clips(self):
-        #channel_id = self.get_channel_id(self.name)
-        #url = "https://mixer.com/api/v1/clips/channels/" + channel_id
-        url = "https://mixer.com/api/v1/clips/channels/51623"
+        channel_id = self.get_channel_id(self.name)
+        url = "https://mixer.com/api/v1/clips/channels/" + channel_id
+        #url = "https://mixer.com/api/v1/clips/channels/51623"
         clip_embeds = []
 
         log.debug(f"Obtaining clip list from URL {url}")
         
-        with aiohttp.ClientSession() as session:
+        """with aiohttp.ClientSession() as session:
             with session.get(url) as r:
                 data = r.text(encoding="utf-8")
         if r.status == 200:
@@ -386,7 +386,7 @@ class MixerStream(Stream):
         elif r.status == 404:
             raise StreamNotFound()
         else:
-            raise APIError()
+            raise APIError()"""
 
     async def is_online(self):
         url = "https://mixer.com/api/v1/channels/" + self.name
