83192b906481f63a7b10fb5697ab1009e4bce7b9
bvasser <bvasser@cloudplus.net>
2020-06-11 16:33:44 -0400

    Update streamtypes.py

--- streamclips/streamtypes.py
+++ streamclips/streamtypes.py
@@ -368,9 +368,11 @@ class MixerStream(Stream):
         #channel_id = self.get_channel_id(self.name)
         #url = "https://mixer.com/api/v1/clips/channels/" + channel_id
         url = "https://mixer.com/api/v1/clips/channels/51623"
-        #clip_embeds = []
+        clip_embeds = []
 
         log.debug("Obtaining clip list from URL " + url)
+
+        return clip_embeds
         
         """with aiohttp.ClientSession() as session:
             with session.get(url) as r:
