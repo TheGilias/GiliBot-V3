d5d90aad35afe2f8eb2f4dfd0d12f4cc32053dc9
bvasser <bvasser@cloudplus.net>
2020-06-11 16:29:39 -0400

    Update streamtypes.py

--- streamclips/streamtypes.py
+++ streamclips/streamtypes.py
@@ -365,7 +365,7 @@ class MixerStream(Stream):
     token_name = None  # This streaming services don't currently require an API key
 
     def get_clips(self):
-        channel_id = self.get_channel_id(self.name)
+        #channel_id = self.get_channel_id(self.name)
         #url = "https://mixer.com/api/v1/clips/channels/" + channel_id
         url = "https://mixer.com/api/v1/clips/channels/51623"
         clip_embeds = []
